b8c9b53318ccec0acb25a61d6466053ffa6e5317
wozhuchenfu <1107889298@qq.com>
2018-11-28 00:45:49 +0800

    springCloud zipkin

--- springCloud-Zipkinserver1/src/main/java/ZipkinApplication.java
+++ springCloud-Zipkinserver1/src/main/java/ZipkinApplication.java
@@ -1,10 +1,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin2.server.internal.EnableZipkinServer;
 
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer//使用中间件收集数据
 public class ZipkinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZipkinApplication.class, args);
