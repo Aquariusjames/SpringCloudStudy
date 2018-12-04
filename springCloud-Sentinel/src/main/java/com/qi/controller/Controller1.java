bcb1daa2c7f82d806c7de0a7d85231382f975a21
qijingyu <qijingyu@ybm100.com>
2018-12-04 14:58:48 +0800

    springCloud 整合 Alibaba sentinel

--- springCloud-Sentinel/src/main/java/com/qi/controller/Controller1.java
+++ springCloud-Sentinel/src/main/java/com/qi/controller/Controller1.java
@@ -6,7 +6,6 @@ import com.qi.entity.User;
 import com.qi.feign.UserFeignClient;
 import org.apache.commons.httpclient.util.ExceptionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
